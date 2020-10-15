324178d9112a5a94b5c9baa0e2011dcabe83151d
dewade22 <dewadwizx@gmail.com>
2020-10-15 12:12:52 +0800

    initial commit
    Initial Commit

---
+++ BISM/Models/MainSystem/ExportImportTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BISM.Models.MainSystem
+{
+    public partial class ExportImportTable
+    {
+        public int CompanyId { get; set; }
+        public string CompanyCode { get; set; }
+        public string NewFieldName { get; set; }
+        public string OldFieldName { get; set; }
+    }
+}
