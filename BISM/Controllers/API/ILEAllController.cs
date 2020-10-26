b8cb936a5700c20df3d8a312cab83410c6cec659
dewade22 <dewadwizx@gmail.com>
2020-10-26 15:03:22 +0800

    perbaikan item yg diselect di frontend

---
+++ BISM/Controllers/API/ILEAllController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BISM.Models.BalimoonBML;
+
+namespace BISM.Controllers.API
+{
+    [Route("api/StockILE")]
+    [ApiController]
+    public class ILEAllController : ControllerBase
+    {
+        private BalimoonBMLContext _dataContext;
+
+        public ILEAllController(BalimoonBMLContext context)
+        {
+            _dataContext = context;
+        }
+        [HttpGet]
+        public IActionResult getALLILE()
+        {
+            var hasil = (from a in _dataContext.ItemLedgerEntry
+                         group a by a.ItemNo into g
+                         select new { itemno = g.Key, total = g.Sum(a => a.Quantity) }).ToList();
+            return new JsonResult(new { result = "OK", data = hasil });
+        }
+    }
+}
